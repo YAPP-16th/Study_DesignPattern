04869f81c811a82c6647f9aa2d10075a52ff5171
denny.k <denny.k@kakaocorp.com>
2020-03-24 11:47:47 +0900

    Rx Swift Initialize

---
+++ iOS/Taein/RxSwift_MVVM_Practice/RxSwift_MVVM_Practice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RxSwift_MVVM_Practice
+//
+//  Created by TaeinKim on 2020/03/24.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
